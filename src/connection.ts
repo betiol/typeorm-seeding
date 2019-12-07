1fe64391bd05e5fe30e570ad5141dff23fae58a0
Nikollas Betiol <nikollasbetiol@gmail.com>
2019-12-07 18:43:34 -0400

    feat: add support to multiple connections

--- src/connection.ts
+++ src/connection.ts
@@ -1,22 +1,31 @@
-import * as path from 'path'
+import * as path from 'path';
 import {
-  Connection,
-  createConnection as createTypeORMConnection,
-  ConnectionOptions as TypeORMConnectionOptions,
-} from 'typeorm'
+	Connection,
+	createConnection as createTypeORMConnection,
+	ConnectionOptions as TypeORMConnectionOptions
+} from 'typeorm';
 
 interface SeedingOptions {
-  readonly factories: string[]
-  readonly seeds: string[]
+	readonly factories: string[];
+	readonly seeds: string[];
 }
 
-export declare type ConnectionOptions = TypeORMConnectionOptions & SeedingOptions
+export declare type ConnectionOptions = TypeORMConnectionOptions & SeedingOptions;
 
-export const getConnectionOptions = async (configPath = 'ormconfig.js'): Promise<ConnectionOptions> => {
-  return require(path.join(process.cwd(), configPath))
-}
+export const getConnectionOptions = async (
+	configPath = 'ormconfig.js'
+): Promise<ConnectionOptions> => {
+	return require(path.join(process.cwd(), configPath));
+};
 
 export const createConnection = async (configPath: string): Promise<Connection> => {
-  const options = await getConnectionOptions(configPath)
-  return createTypeORMConnection(options as TypeORMConnectionOptions)
-}
+	let options = await getConnectionOptions(configPath);
+	if (Array.isArray(options)) {
+		options.forEach((item) => {
+			if (item.name === 'default') {
+				options = item;
+			}
+		});
+	}
+	return createTypeORMConnection(options as TypeORMConnectionOptions);
+};
